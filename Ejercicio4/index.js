b6709cc9f56eafe618f7126101cdad9861be4361
emilynievesb <nemily1710@gmail.com>
2023-05-27 15:33:19 -0500

    feat: :sparkles: Desarrollo de lógica y base frontend ejercicio 4
    Se creó una clase padre, su clase hija Coche y se sobreescribió el método acelerar

---
+++ Ejercicio4/index.js
@@ -0,0 +1,56 @@
+class Vehiculo {
+  marca;
+  modelo;
+  velocidad;
+  constructor({ marca, modelo, velocidad }) {
+    this.modelo = modelo;
+    this.marca = marca;
+    this.velocidad = velocidad;
+  }
+  acelerar() {
+    return `La velocidad es de ${(this.velocidad += 10)}`;
+  }
+}
+const BOTON_VEHICULO = document.getElementById("boton-vehiculo");
+BOTON_VEHICULO.addEventListener("click", (e) => {
+  e.preventDefault();
+  const MARCA = document.getElementById("marca-carro").value;
+  const MODELO = document.getElementById("modelo-carro").value;
+  const VELOCIDAD = document.getElementById("velocidad-carro").value;
+  let vehiculo1 = new Vehiculo({
+    marca: MARCA,
+    modelo: MODELO,
+    velocidad: Number(VELOCIDAD),
+  });
+  const CONTENEDOR = document.getElementById("resultado-vehiculo");
+  CONTENEDOR.innerHTML = `<h2>${vehiculo1.acelerar()}</h2>`;
+});
+class Coche extends Vehiculo {
+  combustible;
+  constructor({ marca, modelo, velocidad, combustible }) {
+    super({ modelo, marca, velocidad });
+    this.combustible = combustible;
+  }
+  acelerar() {
+    return `La velocidad es de ${(this.velocidad += 20)}`;
+  }
+}
+const BOTON_COCHE = document.getElementById("boton-coche");
+BOTON_COCHE.addEventListener("click", (e) => {
+  e.preventDefault();
+  const MARCA = document.getElementById("marca-coche").value;
+  const MODELO = document.getElementById("modelo-coche").value;
+  const VELOCIDAD = document.getElementById("velocidad-coche").value;
+  const COMBUSTIBLE = document.getElementById("combustible-coche").value;
+  let coche1 = new Coche({
+    marca: MARCA,
+    modelo: MODELO,
+    velocidad: Number(VELOCIDAD),
+    combustible: COMBUSTIBLE,
+  });
+  console.log(coche1);
+  console.log();
+
+  const CONTENEDOR = document.getElementById("resultado-coche");
+  CONTENEDOR.innerHTML = `<h2>${coche1.acelerar()}</h2>`;
+});
